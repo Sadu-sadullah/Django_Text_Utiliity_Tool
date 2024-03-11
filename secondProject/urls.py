71b0f5d590139bb9295ef3d73063c3b15f4268f2
Sadullah <104268731+Sadu-sadullah@users.noreply.github.com>
2024-03-11 11:28:32 +0800

    Update urls.py

--- secondProject/urls.py
+++ secondProject/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', views.index, name = 'index'),
+    path('https://django-text-utiliity-tool.vercel.app/', views.index, name = 'index'),
     path('removepunctuations', views.removepunctuations, name = 'removepunctuations'),
 ]
